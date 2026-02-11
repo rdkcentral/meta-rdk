7789fad6ced401a46c351d47955f5f0f81bb0e0d
sindhu-krishnan <102755514+sindhu-krishnan@users.noreply.github.com>
2026-02-11 15:57:03 +0530

    Update time_manager.c

--- recipes-support/chrony/files/time_manager.c
+++ recipes-support/chrony/files/time_manager.c
@@ -16,6 +16,20 @@ void handle_step_req()
 void handle_offset_req()
 {
     printf("This is a handler for offset req\n");
+    if (driver->get_offset(&offset) == 0) {
+    printf("[Daemon] Current Offset: %.9f seconds\n", offset);
+    }
+}
+
+void handle_add_Server_req()
+{
+    printf("This is a handler for adding New Server\n");
+    const char *new_server = "time.google.com";
+    if (driver->add_server(new_server) == 0) {
+        printf("Successfully requested to add server: %s\n", new_server);
+    } else {
+        printf("Failed to add server: %s\n", new_server);
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -24,15 +38,16 @@ int main(int argc, char *argv[]) {
     driver = &chrony_driver_socket;
 
     signal(SIGUSR1, handle_offset_req);
-    signal(SIGUSR2, handle_step_req);
+    #signal(SIGUSR2, handle_step_req);
+    signal(SIGUSR2, handle_add_server_req);
     if (driver->init() != 0) {
         fprintf(stderr, "Failed to initialize driver %s\n", driver->name);
         return 1;
     }
 
     printf("Driver %s initialized successfully.\n", driver->name);
-
-    // Main loop
+    
+    /* Main loop
     while (1) {
         double offset;
         if (driver->get_offset(&offset) == 0) {
@@ -48,6 +63,7 @@ int main(int argc, char *argv[]) {
         }
 
         sleep(5);
+        */
     }
 
     driver->close();
