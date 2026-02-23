5ab85e2e2c683995b4e50988df580f27c537611a
sindhu-krishnan <102755514+sindhu-krishnan@users.noreply.github.com>
2026-02-23 22:55:31 +0530

    Update test_chronyctl.c

--- recipes-support/rdkchronylibctrl/files/test_chronyctl.c
+++ recipes-support/rdkchronylibctrl/files/test_chronyctl.c
@@ -1,52 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include "libchronyctl.h"
 
 void check_err(int err, const char *msg) {
     if (err != CHRONYCTL_SUCCESS) {
         fprintf(stderr, "ERROR: %s: %s (%d)\n", msg, chronyctl_strerror(err), err);
-        /* We don't exit here to try other APIs */
     } else {
         printf("SUCCESS: %s\n", msg);
     }
 }
 
-int main() {
+void print_usage(const char *progname) {
+    printf("Usage: %s <command> [args]\n", progname);
+    printf("Commands:\n");
+    printf("  makestep           - Force a time step\n");
+    printf("  server [host]      - Add a server (default: time.xfinity.com)\n");
+    printf("  delete_server [host]- Delete a server (default: pool.ntp.org)\n");
+    printf("  offset_check       - Get current offset\n");
+}
+
+int main(int argc, char *argv[]) {
     int ret;
     double offset;
 
-    printf("--- libchronyctl Test Application ---\n");
+    if (argc < 2) {
+        print_usage(argv[0]);
+        return 1;
+    }
+
+    const char *cmd = argv[1];
+
+    printf("--- libchronyctl Test Application (%s) ---\n", cmd);
 
     ret = chronyctl_init();
     check_err(ret, "Initialization");
     if (ret != CHRONYCTL_SUCCESS) return 1;
 
-    printf("\n1. Testing get_offset...\n");
-    ret = chronyctl_get_offset(&offset);
-    check_err(ret, "Get Offset");
-    if (ret == CHRONYCTL_SUCCESS) {
-        printf("Current Offset: %.9f seconds\n", offset);
+    if (strcmp(cmd, "offset_check") == 0) {
+        printf("\nTesting get_offset...\n");
+        ret = chronyctl_get_offset(&offset);
+        check_err(ret, "Get Offset");
+        if (ret == CHRONYCTL_SUCCESS) {
+            printf("Current Offset: %.9f seconds\n", offset);
+        }
+    } else if (strcmp(cmd, "makestep") == 0) {
+        printf("\nTesting makestep...\n");
+        ret = chronyctl_makestep();
+        check_err(ret, "Make Step");
+    } else if (strcmp(cmd, "server") == 0) {
+        const char *host = (argc > 2) ? argv[2] : "time.xfinity.com";
+        printf("\nTesting add_server (%s)...\n", host);
+        ret = chronyctl_add_server(host, 6, 10);
+        check_err(ret, "Add Server");
+    } else if (strcmp(cmd, "delete_server") == 0) {
+        const char *host = (argc > 2) ? argv[2] : "pool.ntp.org";
+        printf("\nTesting delete_server (%s)...\n", host);
+        ret = chronyctl_delete_server(host);
+        check_err(ret, "Delete Server");
+    } else {
+        printf("Unknown command: %s\n", cmd);
+        print_usage(argv[0]);
+        chronyctl_cleanup();
+        return 1;
     }
-
-    printf("\n2. Testing makestep...\n");
-    ret = chronyctl_makestep();
-    check_err(ret, "Make Step");
-
-    printf("\n3. Testing add_server (pool.ntp.org)...\n");
-    ret = chronyctl_add_server("time.xfinity.com", 6, 10);
-    check_err(ret, "Add Server");
-
-    printf("\n4. Testing set_poll (pool.ntp.org)...\n");
-    ret = chronyctl_set_poll("pool.ntp.org", 7, 11);
-    check_err(ret, "Set Poll");
-
-    printf("\n5. Testing delete_server (pool.ntp.org)...\n");
-    printf("Note: This might fail if the server address resolved differently than what was added.\n");
-    /* Ideally we should resolve the address first, but for testing we'll try the name if the protocol allows, 
-       or use a known IP. Chronyd often requires IP for deletion. */
-    ret = chronyctl_delete_server("pool.ntp.org");
-    check_err(ret, "Delete Server");
 
     chronyctl_cleanup();
     printf("\n--- Test Finished ---\n");
