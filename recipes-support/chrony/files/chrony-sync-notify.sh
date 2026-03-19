11d879ba7f238c6ff0a8c51f9a6fb89040d20a41
sindhu-krishnan <102755514+sindhu-krishnan@users.noreply.github.com>
2026-03-19 15:34:27 +0530

    Update chrony-sync-notify.sh

--- recipes-support/chrony/files/chrony-sync-notify.sh
+++ recipes-support/chrony/files/chrony-sync-notify.sh
@@ -58,7 +58,7 @@ fi
 
 touch "/tmp/systimemgr/ntp" && log "Created $NTP_FILE"
 #log "Change the permission of $NTP_FILE"
-#chmod 644 "$NTP_FILE"
+chmod 644 "$NTP_FILE"
 log "Permission changed for $NTP_FILE"
 
 
